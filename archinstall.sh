668b5a0f83e20aca1e5227bc1bd3fc3c5bff6a2d
nsutic <nikolasutic1@gmail.com>
2019-02-14 23:20:15 +0100

    Arhc install script scraps and dotfiles backup to server.

---
+++ archinstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+system_clock {
+	echo "Set the time with "
+}
+
+format_partitions {
+}
---
+++ backup_dotfiles.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+copy_dotfiles() {
+	if [ ! -d /home/nikola/Dotfiles ]; then
+		mkdir /home/nikola/Dotfiles
+	fi
+	find /home/nikola -name "\.*" -exec cp -r {} /home/nikola/Dotfiles/ \; &>/dev/null
+}
+
+gen_targz() {
+	cd /home/nikola
+	tar -c /home/nikola/Dotfiles -f /home/nikola/HP15dotfiles.tar
+	gzip --rsyncable /home/nikola/HP15dotfiles.tar
+}
+
+backup_targz() {
+	rsync /home/nikola/HP15dotfiles.tar.gz nikola@marcus:/home/nikola/Backups/
+}
+
+finish() {
+	rm -r /home/nikola/Dotfiles
+	rm /home/nikola/HP15*
+}
+
+copy_dotfiles && 
+gen_targz && 
+backup_targz && 
+finish 
+

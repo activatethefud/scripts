e5e161314942a28eafb871f19205c7ab65a23731
nsutic <nikolasutic1@gmail.com>
2019-02-17 12:21:03 +0100

    Added script to grab images.

---
+++ grabimages/images.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+extract() {
+	if [ ! -d images ]; then
+		mkdir images
+	else
+		rm -r images
+		mkdir images
+	fi
+	grep -P -o "href=\".*?\"" thread | grep -P -o "\".*\.png\"" | sed "s/\"//g; s/\/\///" | uniq  > ./images/urls
+	cd images
+	wget -U mozilla -nc -e robots=off --input-file=urls
+}
+
+html() {
+	wget -U mozilla -e robots=off --output-document=thread $1
+}
+
+html $@
+extract
+
+sxiv ./images/*
+
--- screencast.sh
+++ screencast.sh
@@ -3,7 +3,7 @@
 
 
 if [ -z "$(ps -e | grep ffmpeg)" ]; then
-	ffmpeg -f x11grab -s 1366x768 -i $DISPLAY /home/$(whoami)/Videos/Screencast-$(date +%d%m%Y%T).mp4 &
+	ffmpeg -f x11grab -r 25 -s 1366x768 -i $DISPLAY /home/$(whoami)/Videos/Screencast-$(date +%d%m%Y%T).mp4 & 2>/home/$(whoami)/Logs/ffmpeg.log
 else
 	kill -s SIGQUIT $(ps -e | grep ffmpeg | awk {'print $1'})
 fi

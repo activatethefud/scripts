74723d3c3cb0358c19f71c5feaeb9c4f431e6034
nsutic <nikolasutic1@gmail.com>
2019-02-27 20:18:04 +0100

    More scripts for xrandr boot autoconfigure

--- xrandr.sh
+++ xrandr.sh
@@ -5,4 +5,8 @@ primary="$(xrandr | grep -w primary | awk {'print $1'})"
 
 xrandr --output "$primary" --off
 xrandr --output "$external" --primary --auto
+if [ "$external" = "VGA1" ]; then
+	xrandr --output "$external" --mode '1600x900_60.00'
+	fi
+feh --bg-scale ~/Wallpapers/current.jpg
 

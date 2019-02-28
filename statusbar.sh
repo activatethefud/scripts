1861035b51170e0b308964cda67ae57c7301fc6e
nsutic <nikolasutic1@gmail.com>
2019-02-28 16:54:57 +0100

    More paramaters for status bar.

--- statusbar.sh
+++ statusbar.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-
 while true; do
 # Get variables
 	battemp="$(acpi -t | awk {'print $4'})"
 	homespace="$(df -h | grep -w /home | awk {'print $4'})"
 	rootspace="$(df -h | grep -w / | awk {'print $4'})"
 	ethip="$(ip address show | grep 192 | head -1 | awk {'print $2'})"
+	downloading="$(transmission-remote --auth transmission:transmission -l | grep Downloading | wc -l)"
+	finished="$(transmission-remote --auth transmission:transmission -l | grep Idle | grep Done | wc -l)"
+	idle="$(transmission-remote --auth transmission:transmission -l | grep Idle | grep -v Done | wc -l)"
 	
-	echo -e "$battemp/C | $ethip | $rootspace+$homespace | "$(date -R)" \c"
+	
+# Print bar
+	echo -e "D:$downloading I:$idle F:$finished | $battemp/C | $ethip | $rootspace+$homespace | "$(date -R)" \c"
 	sleep 1
 	
 done

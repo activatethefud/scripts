7bd1a4a2a75798d043c1023cbf91de608b5ac974
nsutic <nikolasutic1@gmail.com>
2019-02-28 17:05:24 +0100

    Fixed randr and improved statusbar to print on startup.

--- statusbar.sh
+++ statusbar.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-while true; do
+printbar(){
 # Get variables
-	battemp="$(acpi -t | awk {'print $4'})"
-	homespace="$(df -h | grep -w /home | awk {'print $4'})"
-	rootspace="$(df -h | grep -w / | awk {'print $4'})"
-	ethip="$(ip address show | grep 192 | head -1 | awk {'print $2'})"
-	downloading="$(transmission-remote --auth transmission:transmission -l | grep Downloading | wc -l)"
-	finished="$(transmission-remote --auth transmission:transmission -l | grep Idle | grep Done | wc -l)"
-	idle="$(transmission-remote --auth transmission:transmission -l | grep Idle | grep -v Done | wc -l)"
+	battemp="$(acpi -t | awk {'print $4'})" 2>/dev/null
+	homespace="$(df -h | grep -w /home | awk {'print $4'})" 2>/dev/null
+	rootspace="$(df -h | grep -w / | awk {'print $4'})" 2>/dev/null
+	ethip="$(ip address show | grep 192 | head -1 | awk {'print $2'})" 2>/dev/null
+	downloading="$(transmission-remote --auth transmission:transmission -l | grep Downloading | wc -l)" 2>/dev/null
+	finished="$(transmission-remote --auth transmission:transmission -l | grep Idle | grep Done | wc -l)" 2>/dev/null
+	idle="$(transmission-remote --auth transmission:transmission -l | grep Idle | grep -v Done | wc -l)" 2>/dev/null
 	
 	
 # Print bar
 	echo -e "D:$downloading I:$idle F:$finished | $battemp/C | $ethip | $rootspace+$homespace | "$(date -R)" \c"
-	sleep 1
 	
+}
+
+printbar
+
+# Loop it
+while true; do
+	printbar
+	sleep 1
 done
--- xrandr.sh
+++ xrandr.sh
@@ -3,10 +3,12 @@
 external="$(xrandr | grep -w connected | grep -v primary | awk {'print $1'})"
 primary="$(xrandr | grep -w primary | awk {'print $1'})"
 
-xrandr --output "$primary" --off
-xrandr --output "$external" --primary --auto
-if [ "$external" = "VGA1" ]; then
-	xrandr --output "$external" --mode '1600x900_60.00'
-	fi
-feh --bg-scale ~/Wallpapers/current.jpg
+if [ ! -z "$external" ];  then
+	xrandr --output "$primary" --off
+	xrandr --output "$external" --primary --auto
+	if [ "$external" = "VGA1" ]; then
+		xrandr --output "$external" --mode '1600x900_60.00'
+		fi
+	feh --bg-scale ~/Wallpapers/current.jpg
 
+fi

e82783e527446eae7108fda40bc86c83605fb8e4
nsutic <nikolasutic1@gmail.com>
2019-03-03 00:33:16 +0100

    Added a script to track overheating; changes to statusbar and bootstrap script start.

---
+++ Startup/overheat.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while true; do
+	if [ "$(acpi -t | awk {'print $4'})" \> "70" ]; then
+		notify-send -t 5000 "High temperature! Possible reasons:" "$(ps -eo comm,pcpu,pid | sort -nr -k2 | head -5)"
+	fi
+	sleep 5
+done
--- statusbar.sh
+++ statusbar.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 printbar(){
 # Get variables
 	battemp="$(acpi -t | awk {'print $4'})" 2>/dev/null
@@ -9,10 +10,13 @@ printbar(){
 	downloading="$(transmission-remote --auth transmission:transmission -l | grep Downloading | wc -l)" 2>/dev/null
 	finished="$(transmission-remote --auth transmission:transmission -l | grep Idle | grep Done | wc -l)" 2>/dev/null
 	idle="$(transmission-remote --auth transmission:transmission -l | grep Idle | grep -v Done | wc -l)" 2>/dev/null
+	batlvl="$(acpi -b | grep -Po "\d+%")"
+	batstate="$(acpi -b | awk {'print $3'} | sed 's/,//g')"
+	ramused="$(free -h | grep Mem | awk {'print $3'})"
 	
 	
 # Print bar
-	echo -e "D:$downloading I:$idle F:$finished | $battemp/C | $ethip | $rootspace+$homespace | "$(date -R)" \c"
+	echo -e "RAM:$ramused | D:$downloading I:$idle F:$finished | $battemp/C BAT:$batlvl | $batstate | $ethip | $rootspace+$homespace | "$(date -R)" \c"
 	
 }
 

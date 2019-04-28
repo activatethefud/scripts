12f6266792445c10837c46b53215682858564b33
nsutic <nikolasutic1@gmail.com>
2019-04-28 23:06:34 +0200

    Deleted old files.

--- organize.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-newdir() {
-	if [ ! -d "$1" ]; then
-		mkdir "$1"
-	fi
-}
-
--- rice-bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-get_github(){
-	mkdir ~/Other
-	git clone https://github.com/nsutic/dotfiles
-}
-
-
-

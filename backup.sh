f63aecdcf1fdcaba58f174b62ade6c8b5a266655
nsutic <nikolasutic1@gmail.com>
2019-04-30 12:35:31 +0200

    Changes to the backup file.

--- backup.sh
+++ backup.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-# This script makes a synchronized copy of your system user's home directory, minus the files in .backup_ignore.
+# This script makes a synchronized copy of your system user's home directory of the files listed in .backup_list
 # The script is meant to be run from anacrontab, in which case it runs as root.
 # The root user needs to have passwordless login to the server, use ssh-copy-id for this.
 
 # Set the internal file separator to newline
 IFS=$'\n'
 
-# Basic information (Edit this for your user)
-user_to_backup="nikola"
+# Work from the user's directory
+cd "$HOME"
+
+# Basic information
 server_user="nikola"
 server_address="marcus" # IP here or /etc/hosts alias
 server_backup_dir="/home/nikola/Backup/$(uname -n)/"
 
-# Change the HOME environment variable to fit the user to backup
-export HOME="/home/$user_to_backup"
-
-# Create .backup_ignore file
-[ ! -f $HOME/.backup_ignore ] && su "$user_to_backup" -c "touch $HOME/.backup_ignore"
+# Create .backup_list file
+[ ! -f ./.backup_list ] && touch ./.backup_list
 
 # Create the backup directory on the remote site
 ssh "$server_user"@"$server_address" "[ -d $server_backup_dir ] || mkdir $server_backup_dir"
@@ -31,7 +30,7 @@ rsync \
 --modify-window=1 \
 --copy-links \
 --delete \
-$(ls -a $HOME | diff --left-column - $HOME/.backup_ignore | grep '^<' | sed "s|<\s|$HOME/|" ) \
+$(sed '' .backup_list)
 "$server_user"@"$server_address":"$server_backup_dir" &&
 
 # Finish

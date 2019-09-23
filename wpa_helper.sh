2957631e3700b32007f32ea8f9af687a5528d1f0
Nikola <nikolasutic1@gmail.com>
2019-09-23 17:21:20 +0200

    wpa_helper.sh changes to fit a service type.

--- wpa_helper.sh
+++ wpa_helper.sh
@@ -5,24 +5,23 @@
 ERR_BAD_USAGE=30
 ERR_NO_INTERFACE=31
 ERR_IF_DOWN=32
-
-if [ -z "$2" ]
-then
-	printf "Bad usage!\\nUsage: <script> <SSID> <PASS>\\n" 1>&2
-	exit $ERR_BAD_USAGE
-fi
+ERR_NO_CONFIG=33
+CONFIG='/etc/wpa_supplicant.conf'
+INTERFACE='wlan1'
 
 kill_check() {
 		pkill -9 wpa_supplicant
 		#pkill -9 dhclient
 }
 
+if [ -n "$2" ]
+then
+
 # Kill running processes before proceeding
 kill_check
 
 SSID="$1"
 PASS="$2"
-INTERFACE='wlan1'
 
 # Interface exists check
 if ! iw dev | grep -q "$INTERFACE"
@@ -31,10 +30,9 @@ then
 	exit $ERR_NO_INTERFACE
 fi
 
-CONFIG='/etc/wpa_supplicant.conf'
 
 ip link set "$INTERFACE" up || exit "$ERR_IF_DOWN"
-wpa_passphrase "$SSID" "$PASS" > "$CONFIG"
+wpa_passphrase "$SSID" "$PASS" 1 > "$CONFIG"
 wpa_supplicant -i "$INTERFACE" -c "$CONFIG" -B
 
 # Renewew DHCP leases
@@ -42,3 +40,31 @@ dhclient -r
 
 # Start dhclient to get an IP address
 dhclient
+
+fi
+
+# No arguments entered case
+if [ -z "$1" ]
+then
+
+# Reuse latest configuration
+if [ -f "$CONFIG" ]
+then
+	kill_check
+	ip link set "$INTERFACE" up || exit "$ERR_IF_DOWN"
+	wpa_supplicant -i "$INTERFACE" -c "$CONFIG" -B
+
+	dhclient -r
+	dhclient
+
+else
+	
+	# No config found, no arguments => Error
+	printf "No configuration file found!\\n" 1>&2
+	exit $ERR_NO_CONFIG
+fi
+
+else
+	printf "Bad usage!\\nUsage: <script> [SSID] [PASS]\\n" 1>&2
+	exit $ERR_NO_CONFIG
+fi
